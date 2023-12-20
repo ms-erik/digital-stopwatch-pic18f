493fe332ba88a63922708c8c7ff2e962b8b576db
ms-erik <erikmelges@usp.br>
2023-12-20 18:58:01 -0300

    Fixed issue with 1s time and starting the display

--- cronometro.c
+++ cronometro.c
@@ -23,7 +23,7 @@ void main(){
     // Configuração do temporizador TWR0
     INTCON.TMR0IE = 1; // Habilita a interrupção associada ao temporizador TMR0
     INTCON.TMR0IF = 0; // Zera a flag.
-    T0CON = 0b00000111; // Registro de controle do temporizador
+    T0CON = 0b00000110; // Registro de controle do temporizador
 
     // Configuração do temporizador TMR1
     PIE1.TMR1IE = 1; // Habilita interrupção do temporizador TMR1
@@ -39,8 +39,6 @@ void main(){
     TMR0H=0xC2;
     TMR0L=0xF7;
 
-    T0CON.TMR0ON = 1; // Inicia o temporizador TMR0
-    T1CON.TMR1ON = 1; // Inicia o temporizador TMR1
 
     while(1);
 }
@@ -50,7 +48,6 @@ void INTERRUPTION_HIGH() iv 0x0008 ics ICS_AUTO {
         // lógica para o primeiro botão (RB0)
         T0CON.TMR0ON = 1; // Inicia o timer TMR0
         T1CON.TMR1ON = 0; // Para o temporizador TMR1
-        //PORTD = 0x00;
         INTCON.INT0IF = 0; // Zera a flag de interrupção externa INTO
     }
 
@@ -58,13 +55,11 @@ void INTERRUPTION_HIGH() iv 0x0008 ics ICS_AUTO {
         // lógica para o segundo botão (RB1)
         T1CON.TMR1ON = 1; // Inicia o temporizador TMR1
         T0CON.TMR0ON = 0; // Para o temporizador TMR0
-        //PORTD = 0x00;
         INTCON3.INT1IF = 0; // Zera a flag de interrupção externa INT1
     }
 
     if (INTCON.TMR0IF) {
         // logica para quando ocorre a interrupção do temporizador TMR0
-     //   T0CON.TMR0ON = 0; // Para o timer TMR0
         TMR0H = 0xC2; // Primeiro byte do TMR0
         TMR0L = 0xF7; // Último byte do TMR0
         INTCON.TMR0IF = 0; // Zera a flag de interrupção do temporizador TMR0
@@ -72,11 +67,12 @@ void INTERRUPTION_HIGH() iv 0x0008 ics ICS_AUTO {
 
     if (PIR1.TMR1IF) {
         // lógica para a interrupção do temporizador TMR1
-        //T1CON.TMR1ON = 0; // Para o temporizador TMR1
         TMR1H = 0x0B; // Primeiro byte do TMR1
         TMR1L = 0xDC; // Último byte do TMR1
         PIR1.TMR1IF = 0; // Zera a flag de interrupção do temporizador TMR1
     }
+    //Atualiza o display de 7 segmentos
         PORTD = display_dictionary[count_array];
         count_array = (count_array >= 9) ? 0 : count_array + 1;
 }
+
